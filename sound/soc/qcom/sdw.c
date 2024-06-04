f429e74c9439dae2a621ac1fd545e138c6055dd4
Manikantan R <quic_manrav@quicinc.com>
2024-06-04 16:20:54 +0530

    ASoC: qcom: sdw: Add handling for WSA Codec DMA TX
    VI feeback path uses WSA_CODEC_DMA_TX lpass ports, add support to this
    dai, in common callbacks.
    
    Signed-off-by: Manikantan R <quic_manrav@quicinc.com>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    (cherry picked from commit 4253c3c626dfed119aa9d3d6379570b76b07cdfb)
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>

--- sound/soc/qcom/sdw.c
+++ sound/soc/qcom/sdw.c
@@ -64,6 +64,7 @@ int qcom_snd_sdw_prepare(struct snd_pcm_substream *substream,
 	switch (cpu_dai->id) {
 	case WSA_CODEC_DMA_RX_0:
 	case WSA_CODEC_DMA_RX_1:
+	case WSA_CODEC_DMA_TX_0:
 	case RX_CODEC_DMA_RX_0:
 	case RX_CODEC_DMA_RX_1:
 	case TX_CODEC_DMA_TX_0:
@@ -113,6 +114,7 @@ int qcom_snd_sdw_hw_params(struct snd_pcm_substream *substream,
 
 	switch (cpu_dai->id) {
 	case WSA_CODEC_DMA_RX_0:
+	case WSA_CODEC_DMA_TX_0:
 	case RX_CODEC_DMA_RX_0:
 	case RX_CODEC_DMA_RX_1:
 	case TX_CODEC_DMA_TX_0:
@@ -141,6 +143,7 @@ int qcom_snd_sdw_hw_free(struct snd_pcm_substream *substream,
 	switch (cpu_dai->id) {
 	case WSA_CODEC_DMA_RX_0:
 	case WSA_CODEC_DMA_RX_1:
+	case WSA_CODEC_DMA_TX_0:
 	case RX_CODEC_DMA_RX_0:
 	case RX_CODEC_DMA_RX_1:
 	case TX_CODEC_DMA_TX_0:
