391acac5f838ccc0a951d44458f0fc73c763b708
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2025-01-31 19:52:59 +0100

    TODO: WIP
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>

--- drivers/gpu/drm/msm/dsi/phy/dsi_phy_7nm.c
+++ drivers/gpu/drm/msm/dsi/phy/dsi_phy_7nm.c
@@ -506,6 +506,10 @@ static int dsi_pll_7nm_vco_prepare(struct clk_hw *hw)
 	if (pll_7nm->slave)
 		dsi_pll_enable_global_clk(pll_7nm->slave);
 
+	writel(0x1, pll_7nm->phy->base + REG_DSI_7nm_PHY_CMN_RBUF_CTRL);
+	if (pll_7nm->slave)
+		writel(0x1, pll_7nm->slave->phy->base + REG_DSI_7nm_PHY_CMN_RBUF_CTRL);
+
 	pr_err("%s:%d AAA CCC DSI PLL%d prepare, vco rate = %llu",
 		__func__, __LINE__,
 		pll_7nm->phy->id, pll_7nm->vco_current_rate);
